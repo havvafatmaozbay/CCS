c36a5a67f5475efdd154dba23de286298e310af3
Havva Fatma Özbay <havvafatma45@gmail.com>
2022-06-18 22:09:04 +0300

    Initial Commit

---
+++ CCS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CCS
+//
+//  Created by Havva Fatma Özbay on 18.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
